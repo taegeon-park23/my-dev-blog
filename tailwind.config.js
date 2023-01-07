aa0026f26cea48b73b7668dc6929cb29f1e26212
taegeon-park23 <justerror23@gmail.com>
2023-01-07 14:05:28 +0000

    chore : Add tailwindcss
    Added dev dependencies
      1. "autoprefixer": "^10.4.13",
      2. "postcss": "^8.4.21",
      3. "tailwindcss": "^3.2.4"
    
    Initialize tailwindcss settings.

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      primary: "#101010",
+      mainback: "#1D0070",
+      white: "#fff",
+      black: "#000",
+      default: "#D2D5DA",
+      number: "#4B5563",
+      subnumber: "#111827",
+    },
+    screens: {
+      sm: { max: "768px", min: "350px" },
+      md: "768px" ,
+      lg: "1080px" ,
+      xl: {min: "1440px"},
+    },
+  },
+  plugins: [],
+}
